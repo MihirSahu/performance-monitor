6226d093c8eff9084841877dd36b8e19f5352d86
Mihir Sahu <2002mihir@gmail.com>
2024-12-16 19:51:24 -0600

    feat: set up IPC

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,9 +1,15 @@
-import {app, BrowserWindow} from "electron";
+import {app, BrowserWindow, ipcMain} from "electron";
 import path from "path";
 import { isDev } from "./util.js";
+import { pollResources, getStaticData } from "./resourceManager.js";
+import { getPreloadPath } from "./pathResolver.js";
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: getPreloadPath(),
+    }
+  });
 
   if (isDev()) {
     mainWindow.loadURL('http://localhost:8080');
@@ -11,4 +17,9 @@ app.on("ready", () => {
   else {
     mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
   }
+
+  pollResources(mainWindow);
+  ipcMain.handle("getStaticData", () => {
+    return getStaticData();
+  })
 });
